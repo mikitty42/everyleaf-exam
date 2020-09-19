ad5a59a572e01cb7e9817d84823e047865857d9e
miki takezawa <mikiaaamiki@yahoo.co.jp>
2020-09-19 11:39:33 +0900

    終了期限のカラム追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -42,7 +42,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:title,:content)
+    params.require(:task).permit(:title,:content,:limit_date)
   end
 
   def set_task
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_05_080008) do
+ActiveRecord::Schema.define(version: 2020_09_19_012503) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_09_05_080008) do
     t.text "content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "limit_date", default: "2015-10-20", null: false
   end
 
 end
