e502beb20099486171f64a3d9c8eb75d61b1d21e
miki takezawa <mikiaaamiki@yahoo.co.jp>
2020-09-22 22:33:56 +0900

    statusカラム追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,5 @@ class Task < ApplicationRecord
   validates :title,presence: true
   validates :content,presence: true
   validates :limit_date,presence: true
+  enum status:{未着手: 0, 着手: 1, 完了: 2  }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_19_012503) do
+ActiveRecord::Schema.define(version: 2020_09_22_131446) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_09_19_012503) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.date "limit_date", default: "2015-10-20", null: false
+    t.integer "status", default: 0, null: false
   end
 
 end
