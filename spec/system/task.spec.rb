1eea605b53ce5ca54ae46404b108410b7afd3043
miki takezawa <mikiaaamiki@yahoo.co.jp>
2020-09-29 14:22:43 +0900

    system specのテスト機能実装

--- spec/system/task.spec.rb
+++ spec/system/task.spec.rb
@@ -8,20 +8,26 @@ RSpec.describe 'タスク管理機能', type: :system do
    context 'タイトルであいまい検索をした場合' do
      it "検索キーワードを含むタスクで絞り込まれる" do
        visit tasks_path
-       # タスクの検索欄に検索ワードを入力する (例: task)
-       click_on ''
+       fill_in 'タイトル検索',with: 'task'
+       click_on 'Search'
        expect(page).to have_content 'task'
      end
    end
    context 'ステータス検索をした場合' do
      it "ステータスに完全一致するタスクが絞り込まれる" do
-       # ここに実装する
-       # プルダウンを選択する「select」について調べてみること
+       visit tasks_path
+       select '着手',from: 'status'
+       expect(page).to have_select('status', selected: '着手')
      end
    end
    context 'タイトルのあいまい検索とステータス検索をした場合' do
      it "検索キーワードをタイトルに含み、かつステータスに完全一致するタスク絞り込まれる" do
-       # ここに実装する
+       visit tasks_path
+       fill_in 'タイトル検索',with: 'task'
+       click_on 'Search'
+       select '着手',from: 'status'
+       expect(page).to have_content 'task'
+       expect(page).to have_select('status', selected: '着手')
      end
    end
  end
@@ -60,17 +66,21 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '一覧表示機能' do
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
+        FactoryBot.create(:task, title: "task")
+        FactoryBot.create(:second_task, title: "sample")
         visit tasks_path
-        expect(page).to have_content 'task1'
-        expect(page).to have_content 'content1'
+        expect(page).to have_content 'task'
+        expect(page).to have_content 'sample'
       end
     end
     context 'タスクが作成日時の降順に並んでいる場合' do
       it '新しいタスクが一番上に表示される' do
+        FactoryBot.create(:task, title: "task")
+        FactoryBot.create(:second_task, title: "sample")
         visit tasks_path
         task_list = all('.task_row')
-        expect(task_list[0]).to have_content 'task2'
-        expect(task_list[1]).to have_content 'task1'
+        expect(task_list[0]).to have_content 'sample'
+        expect(task_list[1]).to have_content 'task'
       end
     end
   end
