b1b0232d1c16c15d5f19daa678645d3b3b87cffb
miki takezawa <mikiaaamiki@yahoo.co.jp>
2020-09-29 16:24:08 +0900

    model specテスト

--- spec/models/task.spec.rb
+++ spec/models/task.spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 RSpec.describe 'タスクモデル機能', type: :system do
+  describe '検索機能' do
+    task = FactoryBot.create(:task, title: 'task')
+    second_task = FactoryBot.create(:second_task, title: "sample")
+    context 'scopeメソッドでタイトルのあいまい検索をした場合' do
+      it "検索キーワードを含むタスクが絞り込まれる" do
+        task1 = Task.create!(title: 'task',content:'content',limit_date: '2020-01-01',status: '完了')
+        tasks = Task.get_by_title('task')
+        expect(tasks).to include task1
+      end
+    end
+  end
+    context 'scopeメソッドでステータス検索をした場合' do
+      it "ステータスに完全一致するタスクが絞り込まれる" do
+        task1 = Task.create!(title: 'task',content:'content',limit_date: '2020-01-01',status: '完了')
+        tasks = Task.get_by_status('完了')
+        expect(tasks).to include task1
+      end
+    end
+    context 'scopeメソッドでタイトルのあいまい検索とステータス検索をした場合' do
+      it "検索キーワードをタイトルに含み、かつステータスに完全一致するタスク絞り込まれる" do
+        task1 = Task.create!(title: 'task',content:'content',limit_date: '2020-01-01',status: '完了')
+        tasks = Task.get_by_title('task')
+        expect(tasks).to include task1
+        task1 = Task.create!(title: 'task',content:'content',limit_date: '2020-01-01',status: '完了')
+        tasks = Task.get_by_status('完了')
+        expect(tasks).to include task1
+      end
+    end
   describe 'バリデーションのテスト' do
     context 'タスクのタイトルが空の場合' do
       it 'バリデーションにひっかる' do
