d17b08993fe8131fb82df98997fa71d00a81bfe5
miki takezawa <mikiaaamiki@yahoo.co.jp>
2020-10-28 15:17:14 +0900

    seedデータ修正

--- Gemfile
+++ Gemfile
@@ -67,3 +67,4 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'kaminari'
 gem 'bcrypt', '3.1.11'
+gem 'faker'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
-5.times do |i|
-    Label.create!(name: "sample#{i + 1}")
-end
+#5.times do |i|
+    #Label.create!(name: "sample#{i + 1}")
+#end
 10.times do |i|
   Task.create!(
     title: "task#{i + 1}",
@@ -11,18 +11,28 @@ end
     user_id: rand(1..11)
   )
 end
-User.create!(
-    id: 1,
-    name: 'admin',
-    email: 'admin@testtest.com',
-    password: '000000',
-    password_confirmation: '000000',
-    admin: 'true',
-)
-10.times do |i|
-  User.create!(
-    name: "sample#{i}",
-    email: "sample#{i}@example.com",
-    password: "password#{i}",
-    password_confirmation: "password#{i}"
-)
+#User.create!(
+    #id: 1,
+    #name: 'admin',
+    #email: 'admin@testtest.com',
+    #password: '000000',
+    #password_confirmation: '000000',
+    #admin: 'true',
+#)
+#10.times do |i|
+  #User.create!(
+    #name: "sample#{i}",
+    #email: "sample#{i}@example.com",
+    #password: "password#{i}",
+    #password_confirmation: "password#{i}"
+#)
+10.times do |n|
+  name = Faker::Games::Pokemon.name
+  email = Faker::Internet.email
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password,
+               )
+end
